91c9305f59d4063dbd8338d649681e7ed3c46745
Igor Amantino <justin.amantino@gmail.com>
2022-02-18 12:55:10 -0300

    estilizando menu e criando as funcionalidades

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,30 +1,67 @@
 import { Menu2 as MenuIcon } from '@styled-icons/remix-fill/Menu2'
 import {
   ShoppingCart as ShoppingCartIcon,
-  Search as SearchIcon
+  Search as SearchIcon,
+  Close as CloseIcon
 } from '@styled-icons/material-outlined'
 import { Logo } from 'components/Logo'
 
-import { Wrapper, IconWrapper, LogoWrapper, IconContent } from './styles'
+import {
+  Wrapper,
+  IconWrapper,
+  LogoWrapper,
+  IconContent,
+  MenuFull,
+  MenuNav,
+  MenuLink,
+  RegisterBox,
+  CreateAccount
+} from './styles'
+import { useState } from 'react'
+import { Button } from 'components/Button'
 
-export const Menu = () => (
-  <Wrapper>
-    <IconWrapper>
-      <MenuIcon aria-label="Open Menu" />
-    </IconWrapper>
+export const Menu = () => {
+  const [isOpen, setIsOpen] = useState(false)
 
-    <LogoWrapper>
-      <Logo hideOnMobile />
-    </LogoWrapper>
-
-    <IconContent>
-      <IconWrapper>
-        <SearchIcon aria-label="Search" />
+  return (
+    <Wrapper>
+      <IconWrapper onClick={() => setIsOpen(true)}>
+        <MenuIcon aria-label="Open Menu" />
       </IconWrapper>
 
-      <IconWrapper>
-        <ShoppingCartIcon aria-label="shopping cart" />
-      </IconWrapper>
-    </IconContent>
-  </Wrapper>
-)
+      <LogoWrapper>
+        <Logo hideOnMobile />
+      </LogoWrapper>
+
+      <IconContent>
+        <IconWrapper>
+          <SearchIcon aria-label="Search" />
+        </IconWrapper>
+
+        <IconWrapper>
+          <ShoppingCartIcon aria-label="shopping cart" />
+        </IconWrapper>
+      </IconContent>
+
+      <MenuFull isOpen={isOpen} aria-hidden={!isOpen}>
+        <CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)} />
+
+        <MenuNav>
+          <MenuLink href="a">Home</MenuLink>
+          <MenuLink href="a">Explore</MenuLink>
+        </MenuNav>
+
+        <RegisterBox>
+          <Button fullWidth size="large">
+            Login in now
+          </Button>
+
+          <span>or</span>
+          <CreateAccount href="/" title="Sign Up">
+            Sign Up
+          </CreateAccount>
+        </RegisterBox>
+      </MenuFull>
+    </Wrapper>
+  )
+}
--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -1,4 +1,7 @@
-import { render, screen } from '@testing-library/react'
+import { getByText, render, screen } from '@testing-library/react'
+import FireEvent from '@testing-library/user-event'
+import { debug } from 'console'
+
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import { Menu } from '.'
@@ -8,7 +11,38 @@ describe('<Menu />', () => {
     renderWithTheme(<Menu />)
 
     expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/Won Games/i)).toBeInTheDocument()
     expect(screen.getByLabelText(/search/i)).toBeInTheDocument()
     expect(screen.getByLabelText(/shopping cart/i)).toBeInTheDocument()
+  })
+
+  it('shoud handle the open/close mobile menu', () => {
+    const { getByRole, debug } = renderWithTheme(<Menu />)
+
+    //selecionar o menuFull
+
+    const menu = getByRole('navigation', { hidden: true })
+
+    //verifica se o menu ta fechado
+
+    expect(menu.getAttribute('aria-hidden')).toBe('true')
+    expect(menu).toHaveStyle({ opacity: 0 })
+
+    //clicar no botao abrir o menu e verificar se ele abriu
+    const ClickMenu = screen.getByLabelText(/Open Menu/i)
+
+    FireEvent.click(ClickMenu)
+
+    debug()
+    expect(menu.getAttribute('aria-hidden')).toBe('false')
+    expect(menu).toHaveStyle({ opacity: 1 })
+
+    //clicar no botao de fechar o menu o menu e verifiicar se ele fechou
+    const ButtonClose = screen.getByLabelText(/Close Menu/i)
+
+    FireEvent.click(ButtonClose)
+
+    expect(menu.getAttribute('aria-hidden')).toBe('true')
+    expect(menu).toHaveStyle({ opacity: 0 })
   })
 })
